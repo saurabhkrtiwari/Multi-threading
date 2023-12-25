74f45ee73c7eb7b73db74eb047550ec41f2a9a8f
saurabhkrtiwari <saurabhkrtiwari@gmail.com>
2023-12-25 13:36:16 +0530

    ThreadCreation

---
+++ src/main/java/MultiThreading/basics/ExecutorServiceTest.java
@@ -0,0 +1,16 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorServiceTest {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Executing Thread");
+            }
+        });
+
+    }
+
+}
