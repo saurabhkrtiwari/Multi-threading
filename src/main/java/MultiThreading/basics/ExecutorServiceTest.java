bf2a1f5956a4fa4f1866f3e98ee2f9fc0bd152e4
saurabhkrtiwari <saurabhkrtiwari@gmail.com>
2024-02-13 17:05:14 +0530

    ThreadCreation

--- src/main/java/MultiThreading/basics/ExecutorServiceTest.java
+++ src/main/java/MultiThreading/basics/ExecutorServiceTest.java
@@ -1,15 +1,14 @@
+package MultiThreading.basics;
+
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ExecutorServiceTest {
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.execute(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Executing Thread");
-            }
-        });
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        final int[] i = {0};
+        executorService.execute(() -> System.out.println("Executing Thread "+ (i[0]++)));
 
     }
 
--- src/main/java/MultiThreading/basics/ThreadCreation.java
+++ src/main/java/MultiThreading/basics/ThreadCreation.java
@@ -7,19 +7,36 @@ public class ThreadCreation {
 
         Runnable thread3 = () -> {
             try {
+
                 Thread.sleep(10L);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
 
             }
             for(int i =0;i<10;i++) {
-                System.out.println("lambda thread "+i);
+                System.out.println("Lambda Thread "+Thread.currentThread().getName());
             }
         };
+        thread1.setName("thread1");
+        thread1.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler(){
+            @Override
+            public void uncaughtException(Thread t,Throwable e){
+                System.out.println("A Critical Error "+t.getName() +" error "+e.getMessage());
+            }
+        });
+        thread2.setName("thread2");
 
+
+
+        thread2.setPriority(Thread.MAX_PRIORITY);
+        thread1.setPriority(Thread.MIN_PRIORITY);
+
+        Thread thread = new Thread(thread3);
+        thread.setName("thread3");
+        thread.setPriority(Thread.NORM_PRIORITY);
+        thread.start();
         thread1.start();
         thread2.start();
-        new Thread(thread3).start();
 
     }
 
@@ -29,14 +46,9 @@ class ExtendThread extends Thread{
 
     @Override
     public void run() {
-        try {
-            Thread.sleep(10L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        for(int i =0;i<10;i++) {
-            System.out.println("Extending thread "+i);
-        }
+
+           throw new RuntimeException("Testing an exception in thread");
+
     }
 }
 
@@ -50,7 +62,7 @@ class RunnableThread implements Runnable{
             throw new RuntimeException(e);
         }
         for(int i =0;i<10;i++) {
-            System.out.println("Runnable thread "+i);
+            System.out.println("Runnable thread "+Thread.currentThread().getName());
         }
     }
 }
